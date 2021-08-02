6261442861b86d452000ebd6dc7eab7a7eb9934b
ana bergese <violetagele22@gmail.com>
2021-08-02 19:41:27 +0200

    Todo list created: user can create task, delete task, mark as complete task. Also user can change color of app with options predefined

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
+import './switcher.scss'
+
+// Importing Components
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  // States
+  const [ inputText, setInputText ] = useState("");
+  const [ todos, setTodos ] = useState([]);
+  const [ status, setStatus ] = useState("all");
+  const [ filteredTodos, setFilteredTodos ] = useState([]);
+  const [ colorTheme, setColorTheme ] = useState("theme-white");
+
+  // USE EFFECT
+  useEffect( () => {
+    filterHandler();
+  }, [ todos, status ]);
+
+  useEffect(() => {
+    // Check for selected theme
+    const currentThemeColor = localStorage.getItem('theme-color');
+    // if found set selected theme value in state
+    if (currentThemeColor) {
+      setColorTheme(currentThemeColor);
+    }
+  }, []);
+
+  // Functions
+  const filterHandler = () => {
+    switch(status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break
+      default:
+        setFilteredTodos(todos);
+        break
+    }
+  };
+
+  const handleClick = (theme) => {
+    setColorTheme(theme);
+    localStorage.setItem('theme-color', theme);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={`App ${colorTheme}`}>
+      <div className="theme-options">
+        <div
+          onClick={() => {handleClick('theme-white')}}
+          id="theme-white"
+          className={`${colorTheme === 'theme-white' ? 'active' : ''}`}
+        />
+        <div
+          onClick={() => {handleClick('theme-blue')}}
+          id="theme-blue"
+          className={`${colorTheme === 'theme-blue' ? 'active' : ''}`}
+
+        />
+        <div
+          onClick={() => {handleClick('theme-orange')}}
+          id="theme-orange"
+          className={`${colorTheme === 'theme-orange' ? 'active' : ''}`}
+        />
+        <div
+          onClick={() => {handleClick('theme-purple')}}
+          id="theme-purple"
+          className={`${colorTheme === 'theme-purple' ? 'active' : ''}`}
+        />
+        <div
+          onClick={() => {handleClick('theme-green')}}
+          id="theme-green"
+          className={`${colorTheme === 'theme-green' ? 'active' : ''}`}
+        />
+        <div
+          onClick={() => {handleClick('theme-black')}}
+          id="theme-black"
+          className={`${colorTheme === 'theme-black' ? 'active' : ''}`}
+        />
+      </div>
+      <header>
+        <h1>What's the plan for today? </h1>
       </header>
+      <Form
+          inputText={inputText}
+          todos={todos}
+          setTodos={setTodos}
+          setInputText={setInputText}
+          setStatus={setStatus}
+      />
+      <TodoList filteredTodos={filteredTodos} setTodos={setTodos} todos={todos}/>
     </div>
   );
 }
