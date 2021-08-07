531ababe81e58eb0e65bbb77206ed63e3a4e79e2
ana bergese <violetagele22@gmail.com>
2021-08-07 13:49:43 +0200

    Changed text of description component

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,15 +5,17 @@ const Description = () => {
     <div className="description">
       <h1>This feature is for users on the Autism spectrum disorder (ASD)</h1>
       <ul>
+        <li><strong>To bouild this feature I follow interational recomendations:</strong></li>
+        <li>Goole Material guidelines, components, and tools that support the best practices of user interface design: https://material.io/</li>
+        <li>Web Content Accessibility Guidelines: https://www.w3.org/TR/WCAG20/</li>
+      </ul>
+      <ul>
+        <li><strong>Some considerations applied:</strong></li>
         <li>Use contrast between font and background.</li>
         <li>Make sure text box is clearly separated from the rest.</li>
-        <li>Use simple graphics. </li>
         <li>Use clear, sans-serif fonts.</li>
         <li>Use clear, large buttons with both icons and text.</li>
-        <li>Allow personalization of: Themes for text background and foreground colors.</li>
-        <li>Use clear, large buttons with both icons and text.</li>
-        <li>Following recomendations of Goole Material is an adaptable system of guidelines, components, and tools that support the best practices of user interface design: https://material.io/</li>
-        <li>Following recomendations of Web Content Accessibility Guidelines: https://www.w3.org/TR/WCAG20/</li>
+        <li>Allow personalization of colors.</li>
       </ul>
       <p><a href="https://icons8.com/icon/61195/todo-list" target="_blank" rel="noreferrer">Todo List icon by Icons8</a></p>
     </div>
